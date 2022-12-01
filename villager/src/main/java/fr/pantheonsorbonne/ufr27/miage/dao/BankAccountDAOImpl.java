ae252917ef3b8703fa0fb32657a86c14aae6c4bd
Teya Didi <dteya@yahoo.com>
2022-12-01 23:22:11 +0100

    fix/

--- villager/src/main/java/fr/pantheonsorbonne/ufr27/miage/dao/BankAccountDAOImpl.java
+++ villager/src/main/java/fr/pantheonsorbonne/ufr27/miage/dao/BankAccountDAOImpl.java
@@ -13,18 +13,12 @@ public class BankAccountDAOImpl implements BankAccountDAO {
     @PersistenceContext(name = "mysql")
     EntityManager em;
 
-
     @Override
     @Transactional
     public boolean creditBankAccount(int amount) {
-
-        System.out.println(
-                em.createQuery("select b from BankAccount b").getResultList().size()
-        );
         em.createQuery("update BankAccount b set b.balance = b.balance + :amount")
                 .setParameter("amount", amount)
                 .executeUpdate();
-        //System.out.println(em.createQuery("select b from BankAccount b where b.owner = '1' ").getSingleResult());
         return true;
     }
 }

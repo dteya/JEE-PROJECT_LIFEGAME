21260e59b9f07f04b65e897ea98e102eb70cf818
Merwane <merwane.zioui@gmail.com>
2022-12-12 20:43:55 +0100

    hotfix/ remove unnecessary project and files

--- ticket-vendor/src/main/java/top/nextnet/service/TicketingService.java
+++
@@ -1,9 +0,0 @@
-package top.nextnet.service;
-
-import java.util.Collection;
-
-public interface TicketingService {
-    Collection<ETicket> fillTicketsWithCustomerInformations(Collection<ETicket> tickets);
-
-    void notifyCreatedTicket(String ticketKey);
-}
